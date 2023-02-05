156c96a2788f6c57f02650ee266125ba7b000de1
ngeorgiev <ngeorgiev.code@gmail.com>
2023-02-06 00:26:15 +0200

    3

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const body = document.body;
+  body.addEventListener('click', () => {
+    body.insertAdjacentHTML("beforeend", `
+    <article class="message">sample</article>
+    <article class="message">sample</article>
+    <article class="message">sample</article>
+    <article class="message">sample</article>
+    <article class="message">sample</article>
+  `)
+  })
   
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
